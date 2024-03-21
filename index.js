cf04a0ccc33c0b2cfe85bc5be6c2cfbfa46c3c08
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-21 12:57:39 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(drivers.length - 2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+        return fare * multiplier;
+    }
+}
+
+const selectDifferentDrivers = (drivers, choice) => {
+    
+    if (choice === returnFirstTwoDrivers){
+        return selectingDrivers[0](drivers);
+    }else {
+        return selectingDrivers[1](drivers);
+    }
+    
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const fareQuintupler = createFareMultiplier(5);
+
+console.log(fareQuintupler);
+
